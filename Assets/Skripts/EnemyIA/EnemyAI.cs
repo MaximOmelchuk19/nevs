6febcba20d4a45c23bd8f1e6d9ff1f23792480ee
MaximOmelchuk19 <maximusomelchuk32@gmail.com>
2025-12-12 21:59:41 +0200

    Initial commit with LFS

---
+++ Assets/Skripts/EnemyIA/EnemyAI.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using UnityEngine.AI;
+using KnightAdventure.Utils;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] private State startingState;
+    [SerializeField] private float roamingDistanceMax = 7f;
+    [SerializeField] private float roamimgDistanceMin = 3f;
+    [SerializeField] private float roamimgTimerMax = 2f;
+
+    private NavMeshAgent navMeshAgent;
+    private State state;
+    private float roamingTime;
+    private Vector3 roamPosition;
+    private Vector3 startingPosition;
+
+    private enum State
+    {
+
+        Roaming
+    }
+
+
+    private void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent.updateRotation = false;
+        navMeshAgent.updateUpAxis = false;
+        state = startingState;
+    }
+
+    private void Update()
+    {
+        switch (state)
+        {
+            default:
+            case State.Roaming:
+                roamingTime -= Time.deltaTime;
+                if (roamingTime <= 0f)
+                {
+                    Roaming();
+                    roamingTime = roamimgTimerMax;
+                }
+                break;
+        }
+    }
+
+    private void Roaming()
+    {
+        startingPosition = transform.position;
+        roamPosition = GetRoamingPosition();
+        ChangeFacingDirection(transform.position, roamPosition);
+        navMeshAgent.SetDestination(roamPosition);
+    }
+
+    private Vector3 GetRoamingPosition()
+    {
+        return startingPosition + Utils.GetRandomDir() * UnityEngine.Random.Range(roamimgDistanceMin, roamingDistanceMax);
+    }
+    private void ChangeFacingDirection(Vector3 sourcePosition, Vector3 targetPosition)
+    {
+        transform.rotation = sourcePosition.x > targetPosition.x ? Quaternion.Euler(0, -180, 0) : Quaternion.Euler(0, 0, 0);
+    }
+}
---
+++ Assets/Skripts/Player1/Player.cs
@@ -0,0 +1,48 @@
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class Player : MonoBehaviour{
+
+
+    public static Player Instance { get; private set; }
+
+    [SerializeField] private float movingSpeed = 10f;
+
+    private Rigidbody2D rb;
+    private float minMuvingSpeed = 0.1f;
+    private bool isRunning = false;
+
+    private void Awake() {
+        Instance = this;
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+
+    private void FixedUpdate() {
+       HandleMovement();
+    }
+
+    private void HandleMovement() {
+        Vector2 inputVector = GameInput.Instance.GetMovementVector();
+        rb.MovePosition(rb.position + inputVector * (movingSpeed * Time.fixedDeltaTime));
+
+        if (Mathf.Abs(inputVector.x) > minMuvingSpeed || Mathf.Abs(inputVector.y) > minMuvingSpeed) {
+            isRunning = true;
+        } else {
+            isRunning = false;
+            
+        }
+
+        
+    }
+   public bool IsRunning()
+    {
+        return isRunning;
+    }
+    public Vector3 GetPlayerPosition()
+    {
+       Vector3 playerScreenPosition = Camera.main.WorldToScreenPoint(transform.position);
+        return playerScreenPosition;
+
+    }
+}
